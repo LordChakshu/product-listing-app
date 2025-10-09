f0a7186eecb2c4d89a3f2ce413661894a2b7f13c
chakshu <chakshukota74@gmail.com>
2025-10-10 00:37:26 +0530

    Routes Setup for favourites | Implemented Lazy Loadinf for Favourites

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { lazy, Suspense } from 'react';
 import './App.css'
 import { FavouritesProvider } from './context/FavouritesContext'
-import Home from './pages/home/home'
+import { Link, Route, BrowserRouter , Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/productList'
+
+const Favourites = lazy(() => import("../src/components/Favourites/Favourites"));
 
 function App() {
 
   return (
     <>
       <FavouritesProvider>
-        <Home />
+        <BrowserRouter >
+        <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+          <Link to="/" style={{ marginRight: "1rem" }}>Products</Link>
+          <Link to="/favourites">Favourites</Link>
+        </nav>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/favourites" element={<Favourites />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter >
       </FavouritesProvider>
     </>
   )
