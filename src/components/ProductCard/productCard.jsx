f5fe5fb2134a9445ea2a1602f68af05bff7fe1b4
chakshu <chakshukota74@gmail.com>
2025-10-09 23:28:27 +0530

    Favourites button added to ProductCard

--- src/components/ProductCard/productCard.jsx
+++ src/components/ProductCard/productCard.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductCard.module.css";
+import filledHeart from '../../assets/red-heart.svg'
+import emptyHeart from '../../assets/empty-heart.svg'
 
 const ProductCard = ({ product }) => {
+  const [isFavourite, setIsFavourite] = useState(false);
   return (
     <div className={styles.card}>
       <img src={product.image} alt={product.title} className={styles.image} />
@@ -9,6 +12,13 @@ const ProductCard = ({ product }) => {
         <h3 className={styles.title}>{product.title}</h3>
         <p className={styles.price}>${product.price}</p>
       </div>
+      <button onClick={() => setIsFavourite(!isFavourite)}>
+        <img
+          src={isFavourite ? filledHeart : emptyHeart}
+          alt="favourite"
+          style={{ width: "20px", height: "20px" }}
+        />
+      </button>
     </div>
   );
 };
